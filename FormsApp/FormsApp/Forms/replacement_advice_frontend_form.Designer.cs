44df52f8efc2b0d5078a4dc31ed98e36a7734636
Viljoen-WIM <viljoen@wimtechnologies.com>
2023-01-09 19:38:11 +0200

    Colour change

--- FormsApp/FormsApp/Forms/replacement_advice_frontend_form.Designer.cs
+++ FormsApp/FormsApp/Forms/replacement_advice_frontend_form.Designer.cs
@@ -429,6 +429,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(1256, 603);
             this.Controls.Add(this.cb_DirectMarketing);
             this.Controls.Add(this.cb_Telephone);
