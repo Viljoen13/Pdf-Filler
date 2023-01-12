7ec1344c35821e0bff639eb0db5e3690124fb5ad
alwyn smit <am.smit16@gmail.com>
2023-01-12 17:59:06 +0200

    ZXZXZX

---
+++ FormsApp/FormsApp/entities/2007_AEB_E_Enity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormsApp
+{
+    public class AEB_E_Enity
+    {
+        public List<string> policyPlanNo { get; set; }
+        public List<string> quotationNo { get; set; }
+        public string name { get; set; }
+        public string id { get; set; }
+        public bool permitCheck { get; set; }
+        public bool productCheck { get; set; }
+        public bool investmentCheck { get; set; }
+        public bool contactYes { get; set; }
+        public bool contactNo { get; set; }
+        public string advisoryFeed { get; set; }
+        public string nameOfEmployer { get; set; }
+        public string nameOfTransferringFund { get; set; }
+    }
+}
